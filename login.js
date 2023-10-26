a0e5074c1af047e40fa7e43c2bc2ccff5819343c
Jayden Hacking <hackingerror404@gmail.com>
2023-10-26 11:15:32 -0600

    Add login.js

---
+++ login.js
@@ -0,0 +1,45 @@
+function preventFormSubmit(event) {
+    event.preventDefault();
+}
+
+function login() {
+    let loginInfo = JSON.parse(localStorage.getItem('loginInfo')) || [];
+
+    const usernamePossible = document.getElementById("username");
+    const passwordPossible = document.getElementById("password");
+
+    for (let li of loginInfo) {
+        if (li.username === usernamePossible.value && li.password === passwordPossible.value) {
+            window.location.href = "home.html";
+        }
+    }
+
+    alert("Incorrect username or password!");
+}
+
+function register() {
+    const usernameEl = document.getElementById("username");
+    if (usernameEl.value === "") {
+        alert("Please select a username!");
+        return;
+    }
+
+    const passwordEl = document.getElementById("password");
+    if (passwordEl.value === "") {
+        alert("Please select a password!");
+        return;
+    }
+
+    let loginInfo = JSON.parse(localStorage.getItem('loginInfo')) || [];
+
+    for (let li of loginInfo) {
+        if (li.username === usernameEl.value) {
+            alert("Username already taken! Please select a new one.");
+            return;
+        }
+    }
+
+    loginInfo.push({username: usernameEl.value, password: passwordEl.value});
+    localStorage.setItem("loginInfo", JSON.stringify(loginInfo));
+    window.location.href = "home.html";
+}
